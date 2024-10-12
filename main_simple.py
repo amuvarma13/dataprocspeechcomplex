aa9f5c0eb53bd0b6e49bf1f5fee8a26256c50edc
Amartya Varma <amartya.varma@gmail.com>
2024-10-11 19:11:04 -0700

    f

--- main_simple.py
+++ main_simple.py
@@ -43,7 +43,7 @@ ds = load_dataset("amuvarma/sentences1")
 
 # Process the dataset (assuming we're using the 'train' split)
 
-ds["train"] = ds["train"].select(range(1))
+ds["train"] = ds["train"].select(range(2))
 processed_ds = process_dataset_with_tts(ds['train'])
 
 # Push the processed dataset to the Hub
