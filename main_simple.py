145be6262c98bba8c16c4c68d82f758b24f96132
Amartya Varma <amartya.varma@gmail.com>
2024-10-11 20:53:55 -0700

    f

--- main_simple.py
+++ main_simple.py
@@ -16,39 +16,42 @@ def process_dataset_with_tts(dataset):
             except Exception as e:
                 print(f"Error processing row: {e}")
 
+        # audio = {"array":[], "sampling_rate":16000}
         audio = None
         thread = threading.Thread(target=process)
         thread.start()
         thread.join(timeout=10)  # Wait for up to 10 seconds
 
         if thread.is_alive():
-            print(f"Row {idx} took too long to process. Setting empty audio array.")
+            print(f"Row {idx} took too long to process. Skipping.")
             persistent_ws.reset_socket()
             sleep(10)
-            audio = []  # Set to empty list if processing takes too long
+            return None
         elif audio is None:
-            print(f"Row {idx} failed to process. Setting empty audio array.")
-            audio = []  # Set to empty list if processing fails 
+            print(f"Row {idx} failed to process.")
+            return None
+        else:
+            row['audio'] = {
+                'array': audio,
+                'sampling_rate': 16000
+            }
+            return row
 
-        row['audio'] = {
-            'array': audio,
-            'sampling_rate': 16000
-        }
-        return row
- 
     # Process the dataset
     processed_dataset = dataset.map(
         process_row_with_timeout,
         num_proc=1,
         with_indices=True,
+        remove_columns=dataset.column_names  # Remove original columns
     )
     print("about to push")
-    processed_dataset.push_to_hub("amuvarma/sentences1-audio-debug-0")   
+    processed_dataset.push("amuvarma/sentences1-audio-debug-0")   
     print("pushed")
     # Cast the 'audio' column to Audio feature
     processed_dataset = processed_dataset.cast_column('audio', Audio(sampling_rate=16000))
     
     return processed_dataset
+
 # Load the dataset
 ds = load_dataset("amuvarma/sentences1")
 
