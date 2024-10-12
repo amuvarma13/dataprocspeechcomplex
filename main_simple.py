fb111ebbc07680041419aaf6276c13657bd8506c
Amartya Varma <amartya.varma@gmail.com>
2024-10-11 22:44:37 -0700

    f

--- main_simple.py
+++ main_simple.py
@@ -1,11 +1,10 @@
 from text_to_audio_numpy import text_to_audio_array
 from datasets import load_dataset, Audio, Dataset
-from emotional_phrases.sad import synonyms, phrases
+from emotional_phrases.angry import synonyms, phrases
 import random
 
-batch_number = 2
 
-selected_emotion = "sad"
+selected_emotion = "angry"
 dataset = Dataset.from_dict({
     "text": phrases
 })
@@ -45,10 +44,10 @@ def process_dataset_with_tts(dataset):
     return processed_dataset
 
 
-# dataset = dataset.select(range(0, 10))
+dataset = dataset.select(range(0, 10))
 processed_ds = process_dataset_with_tts(dataset)
 
 # Push the processed dataset to the Hub
-processed_ds.push_to_hub(f"amuvarma/sentencessaddebug")
+processed_ds.push_to_hub(f"amuvarma/sentencesangrydebug")
 
 print("Done processing dataset.")
